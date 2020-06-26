6d85c0a0d08102cf63b9f91806ae0bcae553c21b
herve <dockerlite@gmail.com>
2020-06-26 16:53:25 +0200

    First init

---
+++ src/main/java/day1.java
@@ -0,0 +1,45 @@
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class day1 {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        caps.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new
+                URL("http://seleniumhub:4444/wd/hub"), caps);
+        driver.get("http://51.68.28.250");
+    }
+    @Test
+    public void searchString() {
+        // find search box and enter search string
+        String pagecontent = driver.getPageSource();
+        String search="JavaLand";
+        if (pagecontent.toLowerCase().indexOf(search.toLowerCase()) != -1) {
+            System.out.println("OK");
+        }else {
+            System.out.println("Error Webpage content");
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
