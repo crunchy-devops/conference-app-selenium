30997a11fad67be767b0385666f7b6252f3bb0a8
Herve <dockerlite@gmail.com>
2020-06-26 17:00:11 +0200

    change ip address

--- src/main/java/day1.java
+++ src/main/java/day1.java
@@ -24,7 +24,7 @@ public class day1 {
         caps.setPlatform(Platform.LINUX);
         driver = new RemoteWebDriver(new
                 URL("http://seleniumhub:4444/wd/hub"), caps);
-        driver.get("http://51.68.28.250");
+        driver.get("http://51.68.28.250:10100/");
     }
     @Test
     public void searchString() {
